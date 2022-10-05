938c988d0c3339e2168fd90718b7fd900d3906e0
lav djuric <liondj007@gmail.com>
2022-10-05 11:25:41 +0200

    Added optional relations Added husky pre commit hooks

--- src/fields/field.ts
+++ src/fields/field.ts
@@ -5,15 +5,22 @@ import { CONSTANTS } from "../util/constants";
 export type FieldOptions<T> = {
     builder?: () => T;
     mapper?: (data: T) => any;
+    nullable?: boolean;
 };
 export type Field<T> = { key: keyof T; type: FieldType; options?: FieldOptions<T> };
-export const field = <T>(type = FieldType.STRING, options?: FieldOptions<T>) => {
+
+export type TypeOrOptions<T> = FieldType | FieldOptions<T>;
+
+export function field<T>(typeOrOptions?: FieldType, options?: FieldOptions<T>): (target: any, propertyKey: string) => void;
+export function field<T>(typeOrOptions?: TypeOrOptions<T>, options?: FieldOptions<T>): (target: any, propertyKey: string) => void {
+    const type = typeof typeOrOptions === "string" ? (typeOrOptions as FieldType) : FieldType.STRING;
+    const fieldOptions = typeof typeOrOptions !== "string" ? (typeOrOptions as FieldOptions<T>) : options;
     return (target: any, propertyKey: string): void => {
-        if ([FieldType.COMPONENT, FieldType.RELATION].includes(type) && !options?.builder) {
+        if ([FieldType.COMPONENT, FieldType.RELATION].includes(type) && !fieldOptions?.builder) {
             throw new Error(`Builder not provided`);
         }
         const fields: Field<T>[] = Reflect.getMetadata(CONSTANTS.FIELDS_KEY, target.constructor) ?? [];
-        fields.push({ key: propertyKey as keyof T, type, options });
+        fields.push({ key: propertyKey as keyof T, type, options: fieldOptions });
         Reflect.defineMetadata(CONSTANTS.FIELDS_KEY, fields, target.constructor);
     };
-};
+}
