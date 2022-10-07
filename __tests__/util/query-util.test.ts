aea06061b10bb687ade120d20bb1793720e53630
lav djuric <liondj007@gmail.com>
2022-10-07 15:15:35 +0200

    Refactored query util String could prove bothersome to maintain, this way it will be easier to pass to UrlSearchParams

--- __tests__/util/query-util.test.ts
+++ __tests__/util/query-util.test.ts
@@ -6,22 +6,36 @@ describe("queryUtil", () => {
         it("should create query params", () => {
             const items = [{value: "ASC", field: "[id]"}, {value: "DESC", field: "[id]"}, {value: "DESC", field: "[id]"}]
             const result = mapQueryItemsToString("sort", items);
-            expect(result).toBe('sort[id]=ASC&sort[id]=DESC&sort[id]=DESC');
+            expect(result[0][0]).toBe('sort[id]');
+            expect(result[0][1]).toBe('ASC');
+            expect(result[1][0]).toBe('sort[id]');
+            expect(result[1][1]).toBe('DESC');
+            expect(result[2][0]).toBe('sort[id]');
+            expect(result[2][1]).toBe('DESC');
         })
         it("should map value array to array of params", () => {
             const items = [{value: "ASC", field: "[id]"}, {value: ["DESC", "ASC"], field: "[id]"}]
             const result = mapQueryItemsToString("sort", items);
-            expect(result).toBe('sort[id]=ASC&sort[id][0]=DESC&sort[id][1]=ASC');
+            expect(result[0][0]).toBe('sort[id]');
+            expect(result[0][1]).toBe('ASC');
+            expect(result[1][0]).toBe('sort[id][0]');
+            expect(result[1][1]).toBe('DESC');
+            expect(result[2][0]).toBe('sort[id][1]');
+            expect(result[2][1]).toBe('ASC');
         })
     })
     describe("createWhereQuery", () => {
         it("should create a where query", () => {
             const result = createWhereQuery({value: "1234", field: "[id]", operator: WhereOperator.EQUALS});
-            expect(result).toBe('filters[id][$eq]=1234')
+            expect(result[0][0]).toBe('filters[id][$eq]');
+            expect(result[0][1]).toBe('1234');
         })
         it("should create a list of values if value is array", () => {
             const result = createWhereQuery({value: ["1234", "4321"], field: "[id]", operator: WhereOperator.IN});
-            expect(result).toBe('filters[id][$in][0]=1234&filters[id][$in][1]=4321');
+            expect(result[0][0]).toBe('filters[id][$in][0]');
+            expect(result[0][1]).toBe('1234');
+            expect(result[1][0]).toBe('filters[id][$in][1]');
+            expect(result[1][1]).toBe('4321');
         })
     })
 })
