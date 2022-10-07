37ebc09dc032ceed8e93e1a8ed47ae6d8f970407
lav djuric <liondj007@gmail.com>
2022-10-07 15:15:35 +0200

    Refactored query util String could prove bothersome to maintain, this way it will be easier to pass to UrlSearchParams

--- src/util/query-util.ts
+++ src/util/query-util.ts
@@ -1,33 +1,33 @@
-import { StrapiObject } from "../objects/strapi-object";
 import { WhereOperator } from "../enums/where-operator";
 
 type QueryItem = { field: string; value: string | string[] };
 type WhereQueryItem = { value: string | string[]; operator: WhereOperator; field: string };
 type SortQueryItem = { field: string; value: "ASC" | "DESC" };
-export type Query<T extends StrapiObject> = { where?: WhereQueryItem | WhereQueryItem[]; sort?: SortQueryItem | SortQueryItem[] };
+export type Query = { where?: WhereQueryItem | WhereQueryItem[]; sort?: SortQueryItem | SortQueryItem[]; relations?: string[] };
 
-export const mapQueryItemsToString = (queryType: string, items: QueryItem[]): string => {
-    const queryParts: string[] = [];
+export const mapQueryItemsToString = (queryType: string, items: QueryItem[]): string[][] => {
+    const queryParts: string[][] = [];
     items.map(({ value, field }) => {
-        if (typeof value === "string") return queryParts.push(`${field}=${value}`);
-        value.forEach((val, index) => queryParts.push(`${field}[${index}]=${val}`));
+        if (typeof value === "string") return queryParts.push([`${queryType}${field}`, value]);
+        value.forEach((val, index) => queryParts.push([`${queryType}${field}[${index}]`, val]));
     });
-    return `${queryType}${queryParts.join(`&${queryType}`)}`;
+    return queryParts;
 };
 
-export const createWhereQuery = (where: WhereQueryItem | WhereQueryItem[]): string => {
+export const createWhereQuery = (where: WhereQueryItem | WhereQueryItem[]): string[][] => {
     const filters = Array.isArray(where) ? where : [where];
     const mappedFilters: QueryItem[] = filters.map(({ field, operator, value }) => ({ field: `${field}[${operator}]`, value }));
     return mapQueryItemsToString("filters", mappedFilters);
 };
 
-export const createSortQuery = (sort: SortQueryItem | SortQueryItem[]) => {
+export const createSortQuery = (sort: SortQueryItem | SortQueryItem[]): string[][] => {
     return mapQueryItemsToString("sort", Array.isArray(sort) ? sort : [sort]);
 };
 
-export const createQuery = <T extends StrapiObject>(query: Query<T>): string => {
-    const queryParams: string[] = [];
-    if (query.where) queryParams.push(createWhereQuery(query.where));
-    if (query.sort) queryParams.push(createSortQuery(query.sort));
-    return queryParams.join("&");
+export const createQuery = (query: Query): string[][] => {
+    const queryParams: string[][] = [];
+    if (query.relations) queryParams.push(...query.relations.map(r => [`populate`, r]));
+    if (query.where) queryParams.push(...createWhereQuery(query.where));
+    if (query.sort) queryParams.push(...createSortQuery(query.sort));
+    return queryParams;
 };
