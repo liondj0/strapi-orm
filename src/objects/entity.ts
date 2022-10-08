ba299405c0be321924a7908c04ebe003c08dc8ff
lav djuric <liondj007@gmail.com>
2022-10-08 19:34:38 +0200

    Removed unnecessary complexity to query param on entity object

--- src/objects/entity.ts
+++ src/objects/entity.ts
@@ -3,11 +3,9 @@ import { createQuery, Query } from "../util/query-util";
 import axios from "axios";
 import { getFlatRelationsForObject } from "../util/relationUtil";
 
-export type FetchEntityOptions = { relations?: string[]; query: Query };
-
 export abstract class Entity extends StrapiObject {
-    async fetch(url: string, query?: FetchEntityOptions) {
-        const queryWithDefaultRelations: Query = { ...query?.query, ...{ relations: query?.relations ?? getFlatRelationsForObject(this.getInstance()) } };
+    async fetch(url: string, query?: Query) {
+        const queryWithDefaultRelations: Query = { ...(query ?? {}), ...{ relations: query?.relations ?? getFlatRelationsForObject(this.getInstance()) } };
         const data = (await axios.get(url, { params: new URLSearchParams(createQuery(queryWithDefaultRelations)) })).data.data;
         if (Array.isArray(data)) return data.map(i => this.parseData(i.attributes));
         return this.parseData(data.attributes);
